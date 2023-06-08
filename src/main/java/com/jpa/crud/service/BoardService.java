7d79f8d8a6f1c41d65b08c4151104e9a7dbf62a1
DESKTOP-JURDD5A\ONLINE-DEV02 <ghtn9398@nate.com>
2023-06-08 10:44:36 +0900

    test1

--- src/main/java/com/jpa/crud/service/BoardService.java
+++ src/main/java/com/jpa/crud/service/BoardService.java
@@ -19,7 +19,7 @@ import java.util.stream.Collectors;
 public class BoardService {
 
     private  final BoardRepository boardRepository;
-    //test12
+    //test121
 
     public Board save(BoardDto  boardDto){
 
